99489a9f6cdb5342080d4e9fe242a6898b0c92ab
Nikola <nikola.r.totev@gmail.com>
2022-01-06 20:35:23 +0200

    Update SensorReadingsDataAccess.cs

--- LoRa_Sensor_Network_Blazor_Server_App/DatabaseLogic/SensorReadingsDataAccess.cs
+++ LoRa_Sensor_Network_Blazor_Server_App/DatabaseLogic/SensorReadingsDataAccess.cs
@@ -4,6 +4,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using LoRa_Sensor_Network_Blazor_Server_App.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic
@@ -21,31 +23,48 @@ namespace LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic
 
         //Gets all sensor readings in a time window from a station using the station ID;
         //The time Window is defined by start and end date. A single date can be chosen by setting startDate = endDate;
-        public List<string> GetEntriesSensorReadingsByStationIDWindowed()
+        public List<DbModel_SensorReadingEntry> GetEntriesSensorReadingsByStationIDWindowed(string id, DateTime start, DateTime end)
         {
             using (IDbConnection connection = new SqlConnection(connectionString))
             {
-
+                return connection.Query<DbModel_SensorReadingEntry>(
+                    "dbo.spSensorData_GetEntriesSensorReadingsByStationIDWindowed @StartDate, @EndDate, @StationID", 
+                    new { StartDate = start, EndDate = end, StationId = id }).ToList();
             }
         }
 
         //Gets all of the readings from all of the stations using a given window.
         //The time Window is defined by start and end date. A single date can be chosen by setting startDate = endDate;
-        public void GetEntriesSensorReadingsWindowed()
+        public List<DbModel_SensorReadingEntry> GetEntriesSensorReadingsWindowed(DateTime start, DateTime end)
         {
-
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<DbModel_SensorReadingEntry>(
+                    "dbo.spSensorData_GetEntriesSensorReadingsWindowed @StartDate, @EndDate", new {StartDate = start, EndDate=end}).ToList();
+            }
         }
 
         //Gets only the latest sensor reading from a station using the station ID;
-        public void GetEntryLatestSensorReadingByStationID()
+        public DbModel_SensorReadingEntry GetEntryLatestSensorReadingByStationID(string id)
         {
-
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return connection.Query<DbModel_SensorReadingEntry>(
+                    "dbo.spSensorData_GetEntryLatestSensorReadingByStationID @StationID", new {StationId = id}).First();
+            }
         }
-        
-        //Gets only the latest readings from all of the stations ;
-        public void GetEntriesLatestSensorReadingsAllStations()
-        {
 
+        //Gets only the latest readings from all of the stations;
+        public void GetEntriesLatestSensorReadingsAllStations(StationInfoDataAccess stationInfo)
+        {
+            List<DbModel_BasicStationInfo> ids = stationInfo.GetEntriesListOfStations();
+
+            List<DbModel_SensorReadingEntry> latestData = new List<DbModel_SensorReadingEntry>();
+
+            foreach (DbModel_BasicStationInfo station  in ids)
+            {
+              latestData.Add(GetEntryLatestSensorReadingByStationID(station.stationID));   
+            }
         }
 
 
@@ -53,7 +72,7 @@ namespace LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic
         //The Window is defined by start and end date. A single date can be chosen by setting startDate = endDate;
         public void GetEntriesListOfMeasurementsByDateAndStationIDWindowed()
         {
-
+            //TODO This is an advanced feature and will be left for later.
         }
     }
 }
