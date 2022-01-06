0f6ce5822bcba2a12d2f271ad6ac9bb66ea525ea
Nikola <nikola.r.totev@gmail.com>
2022-01-06 20:06:06 +0200

    Create DbModel_StationEntry.cs

---
+++ LoRa_Sensor_Network_Blazor_Server_App/Models/DbModel_StationEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoRa_Sensor_Network_Blazor_Server_App.Models
+{
+    public class DbModel_StationEntry
+    {
+        //DO NOT CHANGE PROPERTY NAMES!
+        public string stationID;
+        public string joinEUI;
+        public string devAddr;
+        public string stationName;
+        public double longitude;
+        public double latitude;
+        public int numberOfMessages;
+        public DateTime lastSeen;
+        public string supportedMeasurements;
+        public string dateCreated;
+        //DO NOT CHANGE PROPERTY NAMES!
+    }
+}
