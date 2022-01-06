372bb16e61d1668d54f001dba7890dc36af5fc30
Nikola <nikola.r.totev@gmail.com>
2022-01-06 20:06:03 +0200

    Create DbModel_SensorReadingEntry.cs

---
+++ LoRa_Sensor_Network_Blazor_Server_App/Models/DbModel_SensorReadingEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoRa_Sensor_Network_Blazor_Server_App.Models
+{
+    public class DbModel_SensorReadingEntry
+    {
+        //DO NOT CHANGE PROPERTY NAMES!
+        public string readingID;
+        public string originID;
+        public DateTime timeOfCapture;
+        public string payload;
+        //DO NOT CHANGE PROPERTY NAMES!
+    }
+}
