b6495cf98bd170c7f6b1368ba5446d00b3d61a3a
Nikola <nikola.r.totev@gmail.com>
2022-01-06 19:49:00 +0200

    Update SensorReadingsDataAccess.cs

--- LoRa_Sensor_Network_Blazor_Server_App/DatabaseLogic/SensorReadingsDataAccess.cs
+++ LoRa_Sensor_Network_Blazor_Server_App/DatabaseLogic/SensorReadingsDataAccess.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,13 @@ namespace LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic
 {
     public class SensorReadingsDataAccess
     {
+        string conString = m_Configuration.GetConnectionString("LoraDB");
+
         //Gets all sensor readings in a time window from a station using the station ID;
         //The time Window is defined by start and end date. A single date can be chosen by setting startDate = endDate;
         public void GetEntriesSensorReadingsByStationIDWindowed()
         {
-
+            using(IDbConnection connection = new SqlConnection())
         }
 
         //Gets all of the readings from all of the stations using a given window.
