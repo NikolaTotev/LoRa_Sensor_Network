08973dd19356331242360b2a7689b30e1b4fba60
Nikola <nikola.r.totev@gmail.com>
2022-01-08 10:41:59 +0200

    Create ApiEndpointsSignalData.cs

---
+++ LoRa_Sensor_Network_Blazor_Server_App/APIEndpoints/ApiEndpointsSignalData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoRa_Sensor_Network_Blazor_Server_App.APIEndpoints
+{
+    public class ApiEndpointsSignalData
+    {
+    }
+}
