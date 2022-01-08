f6bc7a8b67985170e1e6c1861f367280283085c7
Nikola <nikola.r.totev@gmail.com>
2022-01-08 10:41:53 +0200

    Create ApiEndpointSensorDataController.cs

---
+++ LoRa_Sensor_Network_Blazor_Server_App/APIEndpoints/ApiEndpointSensorDataController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic;
+using LoRa_Sensor_Network_Blazor_Server_App.Models;
+using LoRa_Sensor_Network_Blazor_Server_App.Services;
+
+namespace LoRa_Sensor_Network_Blazor_Server_App.APIEndpoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorReadingsController : ControllerBase
+    {
+        private SensorReadingsDataAccess m_SensorReadingDbAccess;
+        private StationInfoDataAccess m_StationInfoDbAccess;
+        private DataProcessingService m_DataProcessing;
+        public SensorReadingsController(SensorReadingsDataAccess db, StationInfoDataAccess stationDb)
+        {
+            m_SensorReadingDbAccess = db;
+            m_StationInfoDbAccess = stationDb;
+        }
+
+        [HttpGet]
+        public ApiModel_BasicLatestSensorReadings Get()
+        {
+            List<DbModel_SensorReadingEntry> latestSensorReadings =
+                m_SensorReadingDbAccess.GetEntriesLatestSensorReadingsAllStations(m_StationInfoDbAccess);
+
+
+            List<string> listOfTemperatures = m_DataProcessing.ExtractListOfValuesFromListOfReading(latestSensorReadings, "temp");
+
+            List<string> listOfHumidities = m_DataProcessing.ExtractListOfValuesFromListOfReading(latestSensorReadings, "humid");
+
+            double avgTemp = m_DataProcessing.GenerateAvgFromList(listOfTemperatures);
+            double avgHumidities = m_DataProcessing.GenerateAvgFromList(listOfHumidities);
+
+            ApiModel_BasicLatestSensorReadings data = new ApiModel_BasicLatestSensorReadings();
+
+            return data;
+        }
+    }
+}
