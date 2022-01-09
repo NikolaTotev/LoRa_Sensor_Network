cc5f32b228e7e54416cd14b396041cb021a8bc69
Nikola <nikola.r.totev@gmail.com>
2022-01-09 15:32:37 +0200

    Update ApiEndpointsStationDataControllers.cs

--- LoRa_Sensor_Network_Blazor_Server_App/APIEndpoints/ApiEndpointsStationDataControllers.cs
+++ LoRa_Sensor_Network_Blazor_Server_App/APIEndpoints/ApiEndpointsStationDataControllers.cs
@@ -4,12 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic;
+using LoRa_Sensor_Network_Blazor_Server_App.Models;
 
 namespace LoRa_Sensor_Network_Blazor_Server_App.APIEndpoints
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ApiEndpointsStationDataControllers : ControllerBase
+    public class GetStationListController : ControllerBase
     {
+        private StationInfoDataAccess m_StationInfoDataAccess;
+
+        public GetStationListController(StationInfoDataAccess db)
+        {
+            m_StationInfoDataAccess = db;
+        }
+
+        [HttpGet]
+        public List<DbModel_BasicStationInfo> Get()
+        {
+            List<DbModel_BasicStationInfo> listOfStations =
+                m_StationInfoDataAccess.GetEntriesListOfStations();
+
+            return listOfStations;
+        }
     }
 }
