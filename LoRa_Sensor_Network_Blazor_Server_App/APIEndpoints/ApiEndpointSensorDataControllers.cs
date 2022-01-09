f479db98a74a7a2427b37f3bf066390b5901b5b2
Nikola <nikola.r.totev@gmail.com>
2022-01-09 14:29:52 +0200

    Update ApiEndpointSensorDataControllers.cs

--- LoRa_Sensor_Network_Blazor_Server_App/APIEndpoints/ApiEndpointSensorDataControllers.cs
+++ LoRa_Sensor_Network_Blazor_Server_App/APIEndpoints/ApiEndpointSensorDataControllers.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using LoRa_Sensor_Network_Blazor_Server_App.DatabaseLogic;
 using LoRa_Sensor_Network_Blazor_Server_App.Models;
 using LoRa_Sensor_Network_Blazor_Server_App.Services;
+using Newtonsoft.Json;
 
 namespace LoRa_Sensor_Network_Blazor_Server_App.APIEndpoints
 {
@@ -18,10 +19,11 @@ namespace LoRa_Sensor_Network_Blazor_Server_App.APIEndpoints
         private StationInfoDataAccess m_StationInfoDbAccess;
         private DataProcessingService m_DataProcessing;
 
-        public SensorReadingsController(SensorReadingsDataAccess db, StationInfoDataAccess stationDb)
+        public SensorReadingsController(SensorReadingsDataAccess db, StationInfoDataAccess stationDb, DataProcessingService dataProcessing)
         {
             m_SensorReadingDbAccess = db;
             m_StationInfoDbAccess = stationDb;
+            m_DataProcessing = dataProcessing;
         }
 
         [HttpGet]
@@ -31,16 +33,39 @@ namespace LoRa_Sensor_Network_Blazor_Server_App.APIEndpoints
                 m_SensorReadingDbAccess.GetEntriesLatestSensorReadingsAllStations(m_StationInfoDbAccess);
 
 
-            List<string> listOfTemperatures = m_DataProcessing.ExtractListOfValuesFromListOfReading(latestSensorReadings, "temp");
+            List<string> listOfTemperatures = m_DataProcessing.ExtractListOfValuesFromListOfReading(latestSensorReadings, "temperature");
 
             List<string> listOfHumidities = m_DataProcessing.ExtractListOfValuesFromListOfReading(latestSensorReadings, "humid");
 
             double avgTemp = m_DataProcessing.GenerateAvgFromList(listOfTemperatures);
             double avgHumidities = m_DataProcessing.GenerateAvgFromList(listOfHumidities);
 
-            ApiModel_BasicLatestSensorReadings data = new ApiModel_BasicLatestSensorReadings();
-
+            ApiModel_BasicLatestSensorReadings data = new ApiModel_BasicLatestSensorReadings(avgTemp);
+         
             return data;
+        }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationSensorReadingsWindowedController : ControllerBase
+    {
+        private SensorReadingsDataAccess m_SensorReadingDbAccess;
+    
+        public StationSensorReadingsWindowedController(SensorReadingsDataAccess db)
+        {
+            m_SensorReadingDbAccess = db;
+        }
+
+        [HttpGet]
+        public List<DbModel_SensorReadingEntry> Get(string startDate, string endDate, string stationID)
+        {
+            DateTime start = DateTime.Parse(startDate);
+            DateTime end = DateTime.Parse(endDate);
+            List<DbModel_SensorReadingEntry> sensorReadings =
+                m_SensorReadingDbAccess.GetEntriesSensorReadingsByStationIDWindowed(stationID, start, end);
+
+            return sensorReadings;
         }
     }
 }
