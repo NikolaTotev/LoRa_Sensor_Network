27caf4a23c164e37399409871e6fe70b70ac896c
Ivaylo Petkov <ivailo2000petkov@gmail.com>
2022-01-10 19:26:48 +0200

    Add signalR

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as signalR from "@microsoft/signalr";
 
 function App() {
+  const hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("https://localhost:44374/fetchdata")
+    .configureLogging(signalR.LogLevel.Information)  
+    .build();
+ 
+  // Starts the SignalR connection
+  hubConnection.start().then(a => {
+    // Once started, invokes the sendConnectionId in our ChatHub inside our ASP.NET Core application.
+    if (hubConnection.connectionId) {
+      hubConnection.invoke("sendConnectionId", hubConnection.connectionId);
+    }
+  });  
+ 
+  const SignalRTime: React.FC = () => {      
+    // Sets the time from the server
+    const [time, setTime] = useState<string | null>(null);
+ 
+    useEffect(() => {
+      hubConnection.on("setTime", message => {
+        setTime(message);
+      });     
+    });
+ 
+    return <p>The time is {time}</p>;
+  };
+ 
+  const SignalRClient: React.FC = () => {
+    // Sets a client message, sent from the server
+    const [clientMessage, setClientMessage] = useState<string | null>(null);
+ 
+    useEffect(() => {
+      hubConnection.on("setClientMessage", message => {
+        setClientMessage(message);
+      });
+    });
+ 
+    return <p>{clientMessage}</p>
+  };
+ 
+ 
+  return <><SignalRTime /><SignalRClient /></>;
   return (
     <div className="App">
       <header className="App-header">
