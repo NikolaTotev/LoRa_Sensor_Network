b95e8627576689b02784d0337f52e705b9ff1230
Ivaylo-Valentinov <58509466+Ivaylo-Valentinov@users.noreply.github.com>
2022-01-13 20:11:20 +0200

    Add stationList to stations page

--- client-app/src/pages/Stations.tsx
+++ client-app/src/pages/Stations.tsx
@@ -1,8 +1,20 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import Loading from "../components/Loading";
+import useAsync from "../hooks/useAsync";
+import stationService from "../services/StationService";
 
 export default function Stations() {
+  const { data: stationList, loading, error } = useAsync(() => stationService.getStationList(), []);
+
   return (<>
     <Typography>Stations</Typography>
+    <Loading loading={loading} error={error} >
+      {() => stationList?.map(station => (<>
+        <Typography>
+          {station.stationName}
+        </Typography>
+      </>))}
+    </Loading>
   </>);
 }
--- client-app/src/services/StationService.ts
+++ client-app/src/services/StationService.ts
@@ -6,9 +6,26 @@ export interface LatestSensorReading {
   lastUpdate: string;
 }
 
+export interface Station {
+  dateCreated: string; // "Monday, January 10, 2022"
+  devAddr: string; // "260BA563"
+  joinEUI: string; //"70B3D57ED0032B07"
+  lastSeen: string; //"2022-01-13T20:04:08.153"
+  latitude: number; //43
+  longitude: number; //23
+  numberOfMessages: number; //1
+  stationID: string; //"eui-a8610a3032306f09"
+  stationName: string; //"Station-1"
+  supportedMeasurements: string[]; //    "breathVocEquivalent", "co2Equivalent", "humidity", "iaq", "iaqAccuracy", "pressure", "staticIaq","temperature"]
+}
+
 class StationService {
   async getStationReadings() {
     return httpService.get<LatestSensorReading>('api/SensorReadings');
+  }
+
+  async getStationList() {
+    return httpService.get<Station[]>('api/GetStationList');
   }
 }
 
