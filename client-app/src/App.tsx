84e345c2d50a103509d01cd5dd4d28367d7f11a8
Ivaylo-Valentinov <58509466+Ivaylo-Valentinov@users.noreply.github.com>
2022-01-13 02:04:00 +0200

    Fix handling the default path

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ButtonAppBar from './components/Header';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import { ServerSocketProvider } from './contexts/ServerSocket';
 import Home from './pages/Home';
@@ -12,7 +12,8 @@ export default function App() {
         <CssBaseline />
         <ButtonAppBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<Navigate replace to="/home" />} />
         </Routes>
       </ServerSocketProvider>
     </BrowserRouter>
